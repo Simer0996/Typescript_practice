2fc596a40d94fa5f62c46d53552279887a02f43e
Simer0996 <singhsimer09@gmail.com>
2022-09-14 11:43:36 -0700

    array in typescript

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-function add(n1, n2) {
-    return n1 + n2;
+var person = {
+    name: "leo",
+    age: 18,
+    hobbies: ["Sports", "Cooking"]
+};
+console.log(person.name);
+for (var i = 0; i < person.hobbies.length; i++) {
+    console.log(person.hobbies[i]);
 }
-var number1 = 5;
-var number2 = 2.8;
-var result = add(number1, number2);
-console.log(result);
--- app.ts
+++ app.ts
@@ -1,10 +1,14 @@
-function add(n1: number, n2: number) {
-    return n1 + n2;
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+} = {
+    name: "leo",
+    age: 18,
+    hobbies: ["Sports", "Cooking"]
 }
 
-const number1 = 5;
-const number2 = 2.8;
-
-const result = add(number1, number2);
-
-console.log(result);
+console.log(person.name)
+for (let i = 0; i < person.hobbies.length; i++) {
+    console.log(person.hobbies[i])
+}
