20a506bd1f4c8dfe955c5193d3cbaef5682663f0
Simer <simer@pop-os.localdomain>
2022-09-15 18:19:49 -0700

    adding some changes

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
-function combine(n1, n2) {
+function combine(n1, n2, resultConversion) {
     var result;
-    if (typeof n1 === 'number' && typeof n2 === 'number') {
-        result = n1 + n2;
+    if (typeof n1 === 'number' && typeof n2 === 'number' || resultConversion === 'as-number') {
+        result = +n1 + +n2;
         return result;
     }
     else {
         result = n1.toString() + n2.toString();
     }
     return result;
+    // if (resultConversion === "as-number") {
+    //     return +result;
+    // } else {
+    //     return result.toString()
+    // }
 }
-var combinedAges = combine(30, 26);
+var combinedAges = combine(30, 26, "as-number");
 console.log(combinedAges);
-var combinedNames = combine("max", "Anna");
+var combinedNumbers = combine("30", "26", "as-number");
+console.log(combinedNumbers);
+var combinedNames = combine("Max", "Anna", "as-text");
 console.log(combinedNames);
--- app.ts
+++ app.ts
@@ -1,17 +1,30 @@
-function combine(n1: number | string | boolean, n2: number | string) {
+type combinable = number | string
+type conversionDescription = 'as-number' | 'as-text'
+
+
+function combine(n1: combinable, n2: combinable, resultConversion: conversionDescription) {
     let result
-    if (typeof n1 === 'number' && typeof n2 === 'number') {
-        result = n1 + n2
+    if (typeof n1 === 'number' && typeof n2 === 'number' || resultConversion === 'as-number') {
+        result = +n1 + +n2
         return result
     } else {
         result = n1.toString() + n2.toString()
     }
     return result
+    // if (resultConversion === "as-number") {
+    //     return +result;
+    // } else {
+    //     return result.toString()
+    // }
+
 
 }
 
-const combinedAges = combine(30, 26);
+const combinedAges = combine(30, 26, "as-number");
 console.log(combinedAges)
 
-const combinedNames = combine("max", "Anna");
+const combinedNumbers = combine("30", "26", "as-number");
+console.log(combinedNumbers)
+
+const combinedNames = combine("Max", "Anna", "as-text");
 console.log(combinedNames)
